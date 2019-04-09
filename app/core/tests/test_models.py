78e4c10e7b3c268b55836aacbe6063217a980736
Ishwar Bhandari <es.ishwar.bhandari@gmail.com>
2019-04-09 17:38:30 +0545

    Fix linting with flask8

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -18,7 +19,7 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """ Test the email for a new user is normalized """
         email = 'test@TESTDEV.COM'
-        user = get_user_model().objects.create_user(email,'test123')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
